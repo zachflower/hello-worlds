b80aece3364a75f720ed3c34d232fccd09e91e33
Alex <luongalex06@gmail.com>
2024-10-29 14:03:16 -0600

    I made some additions to be able to actually run the code

---
+++ rust/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5.d
@@ -0,0 +1,5 @@
+/home/alex-luong/Documents/AlexLuong-hello-worlds/rust/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5: src/main.rs
+
+/home/alex-luong/Documents/AlexLuong-hello-worlds/rust/hello_world/target/debug/deps/hello_world-4f2a94dd0fcd87c5.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/home/alex-luong/Documents/AlexLuong-hello-worlds/rust/hello_world/target/debug/hello_world: /home/alex-luong/Documents/AlexLuong-hello-worlds/rust/hello_world/src/main.rs
